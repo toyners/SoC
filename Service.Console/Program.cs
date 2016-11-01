53fdb76b0bf139b3ced1773e118e613dd2fe8ef3
Benjamin Toynbee <toyners@hotmail.com>
2016-11-01 17:45:26 +1100

    Removed events from Logger since they become unbound once the service provider is communicating with the client

--- Service.Console/Program.cs
+++ Service.Console/Program.cs
@@ -3,22 +3,28 @@ namespace Jabberwocky.SoC.Service.Console
 {
   using System;
   using System.ServiceModel;
+  using System.Threading;
   using Logging;
 
   public class Program
   {
     public static void Main(String[] args)
     {
-      Logger.MessageEvent = (message) => { Console.WriteLine(message); };
       var serviceHost = new ServiceHost(typeof(ServiceProvider));
 
       serviceHost.Open();
       Logger.Message("Started...");
-      Console.WriteLine("Any key to exit");
-      Console.ReadKey();
 
-      Logger.Message("Closing...");
-      serviceHost.Close();
+      while (true)
+      {
+        Thread.Sleep(250);
+      }
+      
+      //Console.WriteLine("Any key to exit");
+      //Console.ReadKey();
+      //Console.WriteLine();
+      //Logger.Message("Closing...");
+      //serviceHost.Close();
     }
   }
 }
--- Service/Logging/Logger.cs
+++ Service/Logging/Logger.cs
@@ -5,21 +5,14 @@ namespace Jabberwocky.SoC.Service.Logging
 
   public static class Logger
   {
-    #region Events
-    public static Action<String> MessageEvent;
-
-    public static Action<String> ExceptionEvent;
-    #endregion
-
     #region Methods
     public static void Exception(String message)
     {
-      ExceptionEvent?.Invoke(message);
     }
 
     public static void Message(String message)
     {
-      MessageEvent?.Invoke(message);
+      Console.WriteLine(message);
     }
     #endregion
   }
