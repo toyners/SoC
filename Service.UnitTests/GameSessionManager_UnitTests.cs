ab1b26682fcb14b9cd30e99eabf7ceb3dccfa946
Benjamin Toynbee <toyners@hotmail.com>
2016-11-08 22:05:43 +1100

    Added Services.UnitTests project

---
+++ Service.UnitTests/GameSessionManager_UnitTests.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Service.UnitTests
+{
+  using System;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class GameSessionManager_UnitTests
+  {
+    #region Methods
+    [Test]
+    public void Test()
+    {
+    }
+    #endregion 
+  }
+}
