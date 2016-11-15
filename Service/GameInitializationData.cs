19bc75568013c9b15c5469d4e59ee264c809dc41
Benjamin Toynbee <toyners@hotmail.com>
2016-11-15 21:38:28 +1100

    Refactoring only: Moved classes into files

---
+++ Service/GameInitializationData.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Jabberwocky.SoC.Service
+{
+  using System;
+  using System.Runtime.Serialization;
+
+  [DataContract]
+  public class GameInitializationData
+  {
+    [DataMember]
+    public Byte[] BoardData { get; set; }
+
+    [DataMember]
+    public Byte ColumnCount { get; set; }
+
+    [DataMember]
+    public Byte FirstColumnCount { get; set; }
+  }
+}
---
+++ Service/GameInitializationDataBuilder.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Jabberwocky.SoC.Service
+{
+  using Library;
+
+  public static class GameInitializationDataBuilder
+  {
+    public static GameInitializationData Build(Board board)
+    {
+      // Standard board only
+      var gameData = new GameInitializationData()
+      {
+        ColumnCount = 5,
+        FirstColumnCount = 3
+      };
+
+      //board.Locations[0].Providers
+
+      return gameData;
+    }
+  }
+}
