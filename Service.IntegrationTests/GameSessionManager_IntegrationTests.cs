8a6879a48bf2c0aef8325540bc6390ad36f53381
Benjamin Toynbee <toyners@hotmail.com>
2016-11-24 19:07:32 +1100

    Moved GSM tests to integration project. Upped thread sleep time to 5 seconds to ensure the test has time to finishe

--- Service.IntegrationTests/GameSessionManager_IntegrationTests.cs
+++ Service.IntegrationTests/GameSessionManager_IntegrationTests.cs
@@ -2,6 +2,8 @@
 namespace Service.IntegrationTests
 {
   using System;
+  using System.Threading;
+  using Jabberwocky.SoC.Service;
   using NUnit.Framework;
   using Shouldly;
 
@@ -10,8 +12,44 @@ namespace Service.IntegrationTests
   {
     #region Methods
     [Test]
-    public void Test()
+    public void AddPlayer_AddPlayerToNonFullSession_PlayerAdded()
     {
+      var gameSessionManager = new GameSessionManager();
+      gameSessionManager.StartMatching();
+
+      var client = new TestClient();
+
+      gameSessionManager.AddClient(client);
+
+      Thread.Sleep(5000);
+      gameSessionManager.StopMatching();
+
+      client.GameJoined.ShouldBe(true);
+    }
+
+    [Test]
+    public void AddPlayer_AddEnoughPlayersToFillGame_AllPlayersHaveSameGameToken()
+    {
+      var gameSessionManager = new GameSessionManager(4);
+      gameSessionManager.StartMatching();
+
+      var client1 = new TestClient();
+      var client2 = new TestClient();
+      var client3 = new TestClient();
+      var client4 = new TestClient();
+
+      gameSessionManager.AddClient(client1);
+      gameSessionManager.AddClient(client2);
+      gameSessionManager.AddClient(client3);
+      gameSessionManager.AddClient(client4);
+
+      Thread.Sleep(500);
+      gameSessionManager.StopMatching();
+
+      (client1.GameToken != Guid.Empty &&
+      client1.GameToken == client2.GameToken &&
+      client2.GameToken == client3.GameToken &&
+      client3.GameToken == client4.GameToken).ShouldBe(true);
     }
     #endregion 
   }
---
+++ Service.IntegrationTests/TestClient.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Service.IntegrationTests
+{
+  using System;
+  using Jabberwocky.SoC.Service;
+
+  public class TestClient : IServiceProviderCallback
+  {
+    public Guid GameToken;
+
+    public Boolean GameJoined;
+
+    public void ConfirmGameJoined(Guid gameToken)
+    {
+      this.GameToken = gameToken;
+      this.GameJoined = true;
+    }
+
+    public void ConfirmGameLeft()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void GameInitialization(GameInitializationData gameData)
+    {
+    }
+
+    public void StartTurn(Guid token)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
