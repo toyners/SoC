ca40d6accdc975b34fa95ab3e7054b9274eefe7b
Benjamin Toynbee <toyners@hotmail.com>
2016-12-08 07:27:56 +1100

    Added stub method for new integration test

--- Service.IntegrationTests/GameSessionManager_IntegrationTests.cs
+++ Service.IntegrationTests/GameSessionManager_IntegrationTests.cs
@@ -122,6 +122,12 @@ namespace Service.IntegrationTests
       GameIsFullSoPlayerGetsToPlaceFirstTown(diceRolls, expectedResults);
     }
 
+    [Test]
+    public void AddClient_AddEnoughPlayersToFillGame_AllPlayersGetToPlaceFirstTown()
+    {
+
+    }
+
     private void GameIsFullSoPlayerGetsToPlaceFirstTown(List<UInt32> diceRolls, Boolean[] expectedResults)
     {
       // Arrange
