08fb50e1c1ef6cc618706f50ec52d34130a2c6fd
Benjamin Toynbee <toyners@hotmail.com>
2017-05-07 18:33:48 +1000

    Added notes about the role of the ClientAccount. Added ClientAccount class

---
+++ SoC.Library/ClientAccount.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+
+  public class ClientAccount
+  {
+
+  }
+}
