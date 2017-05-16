f56fea8f7033360c8a15176587970a2eed9cbc22
Benjamin Toynbee <toyners@hotmail.com>
2017-05-16 20:07:36 +1000

    Created GameControllerFactory unit tests for local game controller creation

---
+++ SoC.Library.UnitTests/GameControllerFactory_UnitTests.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class GameControllerFactory_UnitTests
+  {
+    #region Methods
+    [Test]
+    public void Create_NullParameter_ReturnsLocalController()
+    {
+      var gameController = new GameControllerFactory().Create(null);
+      gameController.ShouldBeOfType<LocalGameController>();
+    }
+
+    [Test]
+    public void Create_DefaultGameOptions_ReturnsLocalController()
+    {
+      var gameController = new GameControllerFactory().Create(new GameOptions());
+      gameController.ShouldBeOfType<LocalGameController>();
+    }
+
+    [Test]
+    public void Create_GameOptionsSetToLocalConnection_ReturnsLocalController()
+    {
+      var gameController = new GameControllerFactory().Create(new GameOptions { Connection = Enums.GameConnectionTypes.Local });
+      gameController.ShouldBeOfType<LocalGameController>();
+    }
+    #endregion 
+  }
+}
--- SoC.Library.UnitTests/LocalGameController_UnitTests.cs
+++ SoC.Library.UnitTests/LocalGameController_UnitTests.cs
@@ -11,7 +11,24 @@ namespace Jabberwocky.SoC.Library.UnitTests
   {
     #region Methods
     [Test]
-    public void StartJoiningGame_DefaultLocalGame_GameJoinedForOnePlayerWithAIPlayersInOtherSlots()
+    public void StartJoiningGame_DefaultGameOptions_PlayerDataReturned()
+    {
+      var localGameController = new LocalGameController();
+
+      PlayerBase[] players = null;
+      localGameController.GameJoinedEvent = (PlayerBase[] p) => { players = p; };
+      localGameController.StartJoiningGame(new GameOptions());
+
+      players.ShouldNotBeNull();
+      players.Length.ShouldBe(4);
+      players[0].ShouldBeOfType<PlayerData>();
+      players[1].ShouldBeOfType<PlayerView>();
+      players[2].ShouldBeOfType<PlayerView>();
+      players[3].ShouldBeOfType<PlayerView>();
+    }
+
+    [Test]
+    public void StartJoiningGame_NullGameOptions_PlayerDataReturned()
     {
       var localGameController = new LocalGameController();
 
@@ -25,32 +42,6 @@ namespace Jabberwocky.SoC.Library.UnitTests
       players[1].ShouldBeOfType<PlayerView>();
       players[2].ShouldBeOfType<PlayerView>();
       players[3].ShouldBeOfType<PlayerView>();
-    }
-
-    [Test]
-    [TestCase(1, 3)]
-    public void StartJoiningGame_LocalGameWithExplicitGameFilter_GameJoinedWithPlayerSlotsFilled(Int32 maxPlayers, Int32 maxAIPlayers)
-    {
-      var localGameController = new LocalGameController();
-
-      PlayerBase[] players = null;
-      localGameController.GameJoinedEvent = (PlayerBase[] p) => { players = p; };
-      localGameController.StartJoiningGame(new GameOptions { MaxPlayers = (UInt32)maxPlayers, MaxAIPlayers = (UInt32)maxAIPlayers });
-
-      players.ShouldNotBeNull();
-      players.Length.ShouldBe(maxPlayers + maxAIPlayers);
-
-      var index = 0;
-
-      while (maxPlayers-- > 0)
-      {
-        players[index++].ShouldBeOfType<PlayerData>();
-      }
-
-      while (maxAIPlayers-- > 0)
-      {
-        players[index++].ShouldBeOfType<PlayerView>();
-      }
     }
     #endregion 
   }
