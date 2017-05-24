36151ac822e8ae4260d7f4c518ed2905724c56eb
Benjamin Toynbee <toyners@hotmail.com>
2017-05-24 17:45:15 +1000

    Added unit test class for GameBoardData

---
+++ SoC.Library.UnitTests/GameBoardData_UnitTests.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class GameBoardData_UnitTests
+  {
+    #region Methods
+    [Test]
+    public void Test()
+    {
+    }
+    #endregion 
+  }
+}
