c8fefd6b56fef023f7c9966fa1e7e77fd0349f45
Benjamin Toynbee <toyners@hotmail.com>
2017-06-09 06:51:26 +1000

    Refactoring only: Moved road unit test to their own class

--- SoC.Library.UnitTests/LocalGameController_UnitTests.cs
+++ SoC.Library.UnitTests/LocalGameController_UnitTests.cs
@@ -245,16 +245,4 @@ namespace Jabberwocky.SoC.Library.UnitTests
     }
     #endregion 
   }
-
-  [TestFixture]
-  public class Road_UnitTests
-  {
-    [Test]
-    public void Cstr_LocationsAreSame_ThrowMeaningfulException()
-    {
-      Action action = () => { new Road(0u, 0u); };
-
-      Should.Throw<ArgumentException>(action).Message.ShouldBe("Locations cannot be the same");
-    }
-  }
 }
---
+++ SoC.Library.UnitTests/Road_UnitTests.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class Road_UnitTests
+  {
+    [Test]
+    public void Cstr_LocationsAreSame_ThrowMeaningfulException()
+    {
+      Action action = () => { new Road(0u, 0u); };
+
+      Should.Throw<ArgumentException>(action).Message.ShouldBe("Locations cannot be the same");
+    }
+  }
+}
