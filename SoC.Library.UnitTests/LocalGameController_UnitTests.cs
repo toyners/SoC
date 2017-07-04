6ec387cf60815defb08782ad480bdf432bf4a3cd
Benjamin Toynbee <toyners@hotmail.com>
2017-07-05 06:40:55 +1000

    Refactoring only: Moved MockDice to it's own file

--- SoC.Library.UnitTests/LocalGameController_UnitTests.cs
+++ SoC.Library.UnitTests/LocalGameController_UnitTests.cs
@@ -751,31 +751,4 @@ namespace Jabberwocky.SoC.Library.UnitTests
     }
     #endregion 
   }
-
-  public class MockDice : IDice
-  {
-    private Int32 index;
-    private UInt32[] diceRolls;
-
-    public MockDice(UInt32[] first, params UInt32[][] rest)
-    {
-      var rolls = new List<UInt32>(first);
-      foreach(var sequence in rest)
-      {
-        rolls.AddRange(sequence);
-      }
-
-      this.diceRolls = rolls.ToArray();
-    }
-
-    public UInt32 RollTwoDice()
-    {
-      if (this.index >= this.diceRolls.Length)
-      {
-        throw new IndexOutOfRangeException("No more dice rolls.");
-      }
-
-      return this.diceRolls[this.index++];
-    }
-  }
 }
---
+++ SoC.Library.UnitTests/MockDice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Jabberwocky.SoC.Library.Interfaces;
+
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  public class MockDice : IDice
+  {
+    private Int32 index;
+    private UInt32[] diceRolls;
+
+    public MockDice(UInt32[] first, params UInt32[][] rest)
+    {
+      var rolls = new List<UInt32>(first);
+      foreach (var sequence in rest)
+      {
+        rolls.AddRange(sequence);
+      }
+
+      this.diceRolls = rolls.ToArray();
+    }
+
+    public UInt32 RollTwoDice()
+    {
+      if (this.index >= this.diceRolls.Length)
+      {
+        throw new IndexOutOfRangeException("No more dice rolls.");
+      }
+
+      return this.diceRolls[this.index++];
+    }
+  }
+}
