c6263b52873917a708df3f4d2e96aa0489a94232
Benjamin Toynbee <toyners@hotmail.com>
2017-07-31 07:30:28 +1000

    Added empty MockComputerPlayer class for use in testing

---
+++ SoC.Library.UnitTests/MockComputerPlayer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class MockComputerPlayer
+  {
+  }
+}
