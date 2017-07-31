854c5f8929fc560a65cab99b2d1b3e1081ac67cb
Benjamin Toynbee <toyners@hotmail.com>
2017-07-31 17:59:04 +1000

    Added LocalGameContext class which ties together the Computer player, Dice, GameBoardData and LocalGameController instances. MockComputerPlayer now implementing IComputerPlayer

--- SoC.Library.UnitTests/MockComputerPlayer.cs
+++ SoC.Library.UnitTests/MockComputerPlayer.cs
@@ -6,8 +6,38 @@ namespace Jabberwocky.SoC.Library.UnitTests
   using System.Linq;
   using System.Text;
   using System.Threading.Tasks;
+  using Interfaces;
+  using GameBoards;
 
-  public class MockComputerPlayer
+  public class MockComputerPlayer : IComputerPlayer
   {
+    //private Queue<UInt32> 
+    public MockComputerPlayer(UInt32 firstSettlementLocation, Road firstRoad, UInt32 secondSettlementLocation, Road secondRoad)
+    {
+
+    }
+
+    public Guid Id
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public Road ChooseRoad(GameBoardData gameBoardData)
+    {
+      throw new NotImplementedException();
+    }
+
+    public UInt32 ChooseSettlementLocation(GameBoardData gameBoardData)
+    {
+      throw new NotImplementedException();
+    }
+
+    public PlayerDataView GetDataView()
+    {
+      throw new NotImplementedException();
+    }
   }
 }
