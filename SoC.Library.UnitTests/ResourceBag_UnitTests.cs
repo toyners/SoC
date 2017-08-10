286ab2e53fb48f3086b77caaed032a30d2993ff2
Benjamin Toynbee <toyners@hotmail.com>
2017-08-10 19:08:26 +1000

    Added failing test for adding resources to resource bag

---
+++ SoC.Library.UnitTests/ResourceBag_UnitTests.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class ResourceBag_UnitTests
+  {
+    #region Methods
+    [TestFixtureSetUp]
+    public void SetupBeforeAllTests()
+    {
+    }
+
+    [SetUp]
+    public void SetupBeforeEachTest()
+    {
+    }
+
+    [Test]
+    public void Add_VariousTypes_CountsAreCorrect()
+    {
+      // Arrange
+      var resourceBag = new ResourceBag();
+      var resourceClutch = new ResourceClutch(1, 2, 3, 4, 5);
+
+      // Act
+      resourceBag.Add(resourceClutch);
+
+      // Assert
+      resourceBag.BrickCount.ShouldBe(1);
+      resourceBag.GrainCount.ShouldBe(2);
+      resourceBag.LumberCount.ShouldBe(3);
+      resourceBag.OreCount.ShouldBe(4);
+      resourceBag.WoolCount.ShouldBe(5);
+    }
+    #endregion 
+  }
+}
