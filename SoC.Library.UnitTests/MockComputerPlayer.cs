2e22c5c2346cfffcb71abe426cc126e27e799f3c
Benjamin Toynbee <toyners@hotmail.com>
2017-08-15 19:30:18 +1000

    Refactoring only: Moved MockPlayer into its own file

--- SoC.Library.UnitTests/MockComputerPlayer.cs
+++ SoC.Library.UnitTests/MockComputerPlayer.cs
@@ -84,21 +84,4 @@ namespace Jabberwocky.SoC.Library.UnitTests
     }
     #endregion
   }
-
-  public class MockPlayer : Player
-  {
-    public ResourceBag Resources;
-
-    public MockPlayer(String name) : base(name)
-    {
-    }
-
-    public override Int32 ResourcesCount
-    {
-      get
-      {
-        return this.Resources.Count;
-      }
-    }
-  }
 }
---
+++ SoC.Library.UnitTests/MockPlayer.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+
+  public class MockPlayer : Player
+  {
+    public ResourceBag Resources;
+
+    public MockPlayer(String name) : base(name)
+    {
+    }
+
+    public override Int32 ResourcesCount
+    {
+      get
+      {
+        return this.Resources.Count;
+      }
+    }
+  }
+}
