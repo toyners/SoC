19d39b68368b4cbcc7392e67f7a3d1a9e2910e35
Benjamin Toynbee <toyners@hotmail.com>
2017-09-20 18:09:35 +1000

    Started on code to create and load players in player pool

--- SoC.Library/PlayerPool.cs
+++ SoC.Library/PlayerPool.cs
@@ -3,6 +3,7 @@ namespace Jabberwocky.SoC.Library
 {
   using System;
   using System.IO;
+  using System.Xml;
   using Interfaces;
 
   public class PlayerPool : IPlayerPool
@@ -21,9 +22,26 @@ namespace Jabberwocky.SoC.Library
     public IPlayer CreatePlayer(Stream stream)
     {
       throw new NotImplementedException();
+      /*using (var reader = XmlReader.Create(stream, new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment, CloseInput = false, IgnoreComments = true, IgnoreWhitespace = true }))
+      {
+        while (!reader.EOF)
+        {
+          if (reader.Name == "player" && reader.NodeType == XmlNodeType.Element)
+          {
+            return this.CreatePlayer(reader);
+          }
+
+          reader.Read();
+        }
+      }*/
     }
 
     public IPlayer GetPlayer()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal IPlayer CreatePlayer(XmlReader reader)
     {
       throw new NotImplementedException();
     }
