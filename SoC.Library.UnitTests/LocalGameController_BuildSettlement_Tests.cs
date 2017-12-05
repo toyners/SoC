640430dc4ea73512e9741601424f2d465b0f9ca7
Benjamin Toynbee <toyners@hotmail.com>
2017-12-05 17:23:24 +1100

    Added skeleton test class for LocalGameController.BuildSettlement

---
+++ SoC.Library.UnitTests/LocalGameController_BuildSettlement_Tests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  [Category("All")]
+  [Category("LocalGameController")]
+  public class LocalGameController_BuildSettlement_Tests
+  {
+    #region Methods
+    [Test]
+    public void BuildSettlement_Test()
+    {
+    }
+    #endregion 
+  }
+}
