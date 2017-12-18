e4ff0ecf48b241dc8401d03880b1acd3ea685d2f
Benjamin Toynbee <toyners@hotmail.com>
2017-12-18 17:19:45 +1100

    Added empty PlaceCity test class. Updated namespaces for other test classes

--- SoC.Library.UnitTests/GameBoardData_Tests/GameBoardDataTestBase.cs
+++ SoC.Library.UnitTests/GameBoardData_Tests/GameBoardDataTestBase.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Jabberwocky.SoC.Library.UnitTests
+namespace Jabberwocky.SoC.Library.UnitTests.GameBoardData_Tests
 {
   using System;
 
---
+++ SoC.Library.UnitTests/GameBoardData_Tests/GameBoardData_PlaceCity_UnitTests.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests.GameBoardData_Tests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class GameBoardData_PlaceCity_UnitTests
+  {
+    #region Methods
+    [Test]
+    public void Test()
+    {
+    }
+    #endregion 
+  }
+}
