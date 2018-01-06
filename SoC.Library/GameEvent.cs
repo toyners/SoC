22627404f166931000683ac90a2a2e3cf20b56bb
Benjamin Toynbee <toyners@hotmail.com>
2018-01-07 10:00:21 +1100

    Refactoring only: Moved PlayerWithLargestArmyChangedEvent into its own file

--- SoC.Library/GameEvent.cs
+++ SoC.Library/GameEvent.cs
@@ -47,35 +47,4 @@ namespace Jabberwocky.SoC.Library
   {
     public PlayKnightCardEvent(Guid playerId) : base(playerId) { }
   }
-
-  public class PlayerWithLargestArmyChangedEvent : GameEvent
-  {
-    #region Fields
-    public readonly Guid PreviousPlayerWithLargestArmyId;
-    #endregion
-
-    #region Construction
-    public PlayerWithLargestArmyChangedEvent(Guid playerId, Guid previousPlayerWithLargestArmyId) : base(playerId)
-    {
-      this.PreviousPlayerWithLargestArmyId = previousPlayerWithLargestArmyId;
-    }
-    #endregion
-
-    #region Methods
-    public override Boolean Equals(Object obj)
-    {
-      if (!base.Equals(obj))
-      {
-        return false;
-      }
-
-      return this.PreviousPlayerWithLargestArmyId == ((PlayerWithLargestArmyChangedEvent)obj).PreviousPlayerWithLargestArmyId;
-    }
-
-    public override Int32 GetHashCode()
-    {
-      return base.GetHashCode();
-    }
-    #endregion
-  }
 }
---
+++ SoC.Library/PlayerWithLargestArmyChangedEvent.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+  using System;
+
+  public class PlayerWithLargestArmyChangedEvent : GameEvent
+  {
+    #region Fields
+    public readonly Guid PreviousPlayerWithLargestArmyId;
+    #endregion
+
+    #region Construction
+    public PlayerWithLargestArmyChangedEvent(Guid playerId, Guid previousPlayerWithLargestArmyId) : base(playerId)
+    {
+      this.PreviousPlayerWithLargestArmyId = previousPlayerWithLargestArmyId;
+    }
+    #endregion
+
+    #region Methods
+    public override Boolean Equals(Object obj)
+    {
+      if (!base.Equals(obj))
+      {
+        return false;
+      }
+
+      return this.PreviousPlayerWithLargestArmyId == ((PlayerWithLargestArmyChangedEvent)obj).PreviousPlayerWithLargestArmyId;
+    }
+
+    public override Int32 GetHashCode()
+    {
+      return base.GetHashCode();
+    }
+    #endregion
+  }
+}
