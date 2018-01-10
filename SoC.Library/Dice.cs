85b2fa34de3285a4ea97af8205f9338ad26d4595
Benjamin Toynbee <toyners@hotmail.com>
2018-01-10 17:00:19 +1100

    Refactoring only: Added regions

--- SoC.Library/Dice.cs
+++ SoC.Library/Dice.cs
@@ -6,13 +6,17 @@ namespace Jabberwocky.SoC.Library
 
   public class Dice : INumberGenerator
   {
+    #region Fields
     private Random random = new Random();
+    #endregion
 
+    #region Methods
     public UInt32 GetRandomNumber(Int32 exclusiveMaximum)
     {
       return (UInt32)this.random.Next(0, exclusiveMaximum);
     }
 
     public UInt32 RollTwoDice() { return (UInt32)(this.random.Next(1, 6) + this.random.Next(1, 6)); }
+    #endregion
   }
 }
