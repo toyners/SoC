972f0c15b2fca3b456ee99c46539c9bf5bf911e8
Benjamin Toynbee <toyners@hotmail.com>
2018-01-18 20:58:24 +1100

    Added transaction struct detailing the receiving player, the giving player and the resources being traded

---
+++ SoC.Library/Transaction.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public struct Transaction
+  {
+    public readonly Guid ReceivingPlayerId;
+    public readonly Guid GivingPlayerId;
+    public readonly ResourceClutch Resources;
+
+    public Transaction(Guid receivingPlayerId, Guid givingPlayerId, ResourceClutch resources)
+    {
+      this.ReceivingPlayerId = receivingPlayerId;
+      this.GivingPlayerId = givingPlayerId;
+      this.Resources = resources;
+    }
+  }
+}
