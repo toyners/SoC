739c653264be488fc8ee056a33bb0e56d75ea749
Benjamin Toynbee <toyners@hotmail.com>
2018-02-21 17:22:50 +1100

    Added static class providing AI helper methods

---
+++ SoC.Library/AI.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+  using System;
+  using GameBoards;
+
+  public static class AI
+  {
+    public static UInt32[] GetPossibleSettlementLocationsForBestReturningResourceOfType(GameBoard gameBoard, ResourceTypes resourceType, out Int32 productionFactor)
+    {
+      throw new NotImplementedException();
+    }
+
+    public static UInt32[] GetRouteFromPlayerInfrastructureToLocation(GameBoard gameBoard, UInt32 location, Guid playerId)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
