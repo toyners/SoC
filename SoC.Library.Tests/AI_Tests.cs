997e957aa0c5569bfa4bb19d538fa808cff076ff
Benjamin Toynbee <toyners@hotmail.com>
2018-02-24 20:35:49 +1100

    Added empty test class for AI

---
+++ SoC.Library.Tests/AI_Tests.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Jabberwocky.SoC.Library.UnitTests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class AI_Tests
+  {
+    #region Methods
+    [TestFixtureSetUp]
+    public void SetupBeforeAllTests()
+    {
+    }
+
+    [SetUp]
+    public void SetupBeforeEachTest()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+    }
+    #endregion 
+  }
+}
