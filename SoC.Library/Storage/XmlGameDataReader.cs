03c403f74406216fa5f795144ab6f9ca500d1711
Benjamin Toynbee <toyners@hotmail.com>
2018-08-13 07:08:31 +1000

    XmlGameDataReader wip

--- SoC.Library/Storage/XmlGameDataReader.cs
+++ SoC.Library/Storage/XmlGameDataReader.cs
@@ -4,6 +4,7 @@ namespace Jabberwocky.SoC.Library.Storage
   using System.Collections.Generic;
   using System.IO;
   using System.Xml;
+  using System.Xml.Linq;
 
   public class XmlGameDataReader : IGameDataReader<GameDataSectionKeys, GameDataValueKeys, ResourceTypes>
   {
@@ -11,6 +12,29 @@ namespace Jabberwocky.SoC.Library.Storage
 
     public XmlGameDataReader(MemoryStream stream)
     {
+      this.sections = new Dictionary<GameDataSectionKeys, XmlGameDataSection>();
+
+      using (var reader = XmlReader.Create(stream, new XmlReaderSettings { CloseInput = false, IgnoreWhitespace = true, IgnoreComments = true }))
+      {
+        while (!reader.EOF)
+        {
+          if (reader.Name == "resources" && reader.NodeType == XmlNodeType.Element)
+          {
+            if (!this.sections.TryGetValue(GameDataSectionKeys.GameBoard, out var section))
+            {
+              section = new XmlGameDataSection(null);
+              this.sections.Add(GameDataSectionKeys.GameBoard, section);
+            }
+
+            reader.ReadElementContentAsString();
+          }
+
+          if (reader.Name == "production" && reader.NodeType == XmlNodeType.Element)
+          {
+
+          }
+        }
+      }
     }
 
     public XmlGameDataReader(XmlReader stream)
