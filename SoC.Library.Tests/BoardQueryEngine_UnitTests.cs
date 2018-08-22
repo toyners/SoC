de896a2d61ebb61ada3ac328c5eb0ae1cfd31591
Benjamin Toynbee <toyners@hotmail.com>
2018-08-22 17:21:21 +1000

    Added testcases where a settlement is placed in different locations with varying differences in the resulting locations

--- SoC.Library.Tests/BoardQueryEngine_UnitTests.cs
+++ SoC.Library.Tests/BoardQueryEngine_UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace Jabberwocky.SoC.Library.UnitTests
 {
+  using System;
   using Jabberwocky.SoC.Library.GameBoards;
   using Jabberwocky.SoC.Library.UnitTests.Extensions;
   using NUnit.Framework;
@@ -15,7 +16,6 @@ namespace Jabberwocky.SoC.Library.UnitTests
     public void GetLocationsWithBestYield_FirstLocationFromEmptyBoard_ReturnsExpectedLocation()
     {
       var gameBoard = new GameBoard(BoardSizes.Standard);
-
       var queryEngine = new BoardQueryEngine(gameBoard);
 
       var results = queryEngine.GetLocationsWithBestYield(1);
@@ -27,12 +27,29 @@ namespace Jabberwocky.SoC.Library.UnitTests
     public void GetLocationsWithBestYield_FirstFiveLocationsFromEmptyBoard_ReturnsExpectedLocations()
     {
       var gameBoard = new GameBoard(BoardSizes.Standard);
-
       var queryEngine = new BoardQueryEngine(gameBoard);
 
       var results = queryEngine.GetLocationsWithBestYield(5);
 
       results.ShouldContainExact(new[] { 31u, 30u, 43u, 44u, 19u });
+    }
+
+    [Test]
+    [TestCase(31u, 43u, 44u, 19u, 18u, 12u)]
+    [TestCase(30u, 43u, 44u, 19u, 18u, 12u)]
+    [TestCase(43u, 31u, 30u, 19u, 18u, 12u)]
+    [TestCase(44u, 31u, 30u, 19u, 18u, 12u)]
+    [TestCase(19u, 31u, 30u, 43u, 44u, 12u)]
+    [TestCase(18u, 31u, 30u, 43u, 44u, 12u)]
+    public void GetLocationsWithBestYield_FiveLocationsWhenBestLocationIsTaken_ReturnsExpectedLocations(UInt32 settlementLocation, UInt32 firstLocation, UInt32 secondLocation, UInt32 thirdLocation, UInt32 fourthLocation,  UInt32 fifthLocation)
+    {
+      var gameBoard = new GameBoard(BoardSizes.Standard);
+      gameBoard.PlaceSettlement(Guid.NewGuid(), settlementLocation);
+      var queryEngine = new BoardQueryEngine(gameBoard);
+
+      var results = queryEngine.GetLocationsWithBestYield(5);
+
+      results.ShouldContainExact(new[] { firstLocation, secondLocation, thirdLocation, fourthLocation, fifthLocation });
     }
   }
 }
