d9a5494e7a6162051ef783cbae1bff3d0226e928
Benjamin Toynbee <toyners@hotmail.com>
2018-08-23 21:47:51 +1000

    Loading hex and location information during cstr

--- SoC.Library/GameBoards/BoardQueryEngine.cs
+++ SoC.Library/GameBoards/BoardQueryEngine.cs
@@ -3,14 +3,40 @@ namespace Jabberwocky.SoC.Library.GameBoards
 {
   using System;
   using System.Collections.Generic;
+  using System.IO;
+  using System.Reflection;
 
   public class BoardQueryEngine
   {
     private GameBoard board;
+    private readonly List<UInt32>[] locationInformation;
+    private readonly Tuple<ResourceTypes?, UInt32>[] hexInformation;
 
     public BoardQueryEngine(GameBoard board)
     {
       this.board = board;
+      this.hexInformation = this.board.GetHexInformation();
+      this.locationInformation = new List<UInt32>[GameBoard.StandardBoardLocationCount];
+
+      var names = this.GetType().GetTypeInfo().Assembly.GetManifestResourceNames();
+
+      using (var stream = this.GetType().GetTypeInfo().Assembly.GetManifestResourceStream("Jabberwocky.SoC.Library.GameBoards.Locations.txt"))
+      {
+        using (var streamReader = new StreamReader(stream))
+        {
+          var index = 0;
+          while (!streamReader.EndOfStream)
+          {
+            this.locationInformation[index] = new List<UInt32>();
+            foreach(var hexId in streamReader.ReadLine().Split(','))
+            {
+              this.locationInformation[index].Add(UInt32.Parse(hexId));
+            }
+
+            index++;
+          }
+        }
+      }
     }
 
     /// <summary>
@@ -20,32 +46,6 @@ namespace Jabberwocky.SoC.Library.GameBoards
     public UInt32[] GetLocationsWithBestYield(UInt32 count)
     {
       var result = new UInt32[count];
-
-      var hexInformation = this.board.GetHexInformation();
-      var locationInformation = new List<UInt32>[GameBoard.StandardBoardLocationCount];
-      var locationIndex = 0;
-      for (var hexIndex = 0; hexIndex < hexInformation.Length; hexIndex++)
-      {
-
-      }
-
-      // Hex 0
-      locationInformation[0] = new List<uint> { 0 };
-      locationInformation[1] = new List<uint> { 0 };
-      locationInformation[2] = new List<uint> { 0, 1 };
-      locationInformation[3] = new List<uint> { 1 };
-      locationInformation[4] = new List<uint> { 1, 2 };
-      locationInformation[5] = new List<uint> { 2 };
-      locationInformation[6] = new List<uint> { 2 };
-      locationInformation[7] = new List<uint> { 3 };
-      locationInformation[8] = new List<uint> { 0, 3 };
-      locationInformation[9] = new List<uint> { 0, 3, 4 };
-      locationInformation[10] = new List<uint> { 0, 1, 5 };
-      locationInformation[11] = new List<uint> { 1, 4, 5 };
-      locationInformation[12] = new List<uint> { 1, 2, 5 };
-      locationInformation[13] = new List<uint> { 2, 5, 6 };
-      locationInformation[14] = new List<uint> { 2, 6 };
-      locationInformation[15] = new List<uint> { 6 };
 
       if (count == 1)
       {
