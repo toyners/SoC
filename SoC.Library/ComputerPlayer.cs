192cad97a218cb9172edab52432e0ac72d3c8e94
Benjamin Toynbee <toyners@hotmail.com>
2018-09-03 18:53:22 +1000

    Added DecisionMaker class for use in ComputerPlayer

--- SoC.Library/ComputerPlayer.cs
+++ SoC.Library/ComputerPlayer.cs
@@ -316,4 +316,39 @@ namespace Jabberwocky.SoC.Library
     }
     #endregion
   }
+
+  public class DecisionMaker
+  {
+    private INumberGenerator numberGenerator;
+    private List<int> decisionTable = new List<int>();
+
+    public DecisionMaker(INumberGenerator numberGenerator)
+    {
+      this.numberGenerator = numberGenerator;
+    }
+
+    public void Reset()
+    {
+      this.decisionTable.Clear();
+    }
+
+    public void AddDecision(int id, uint multiplier = 1)
+    {
+      if (this.decisionTable.Count + multiplier > this.decisionTable.Capacity)
+      {
+        this.decisionTable.Capacity = this.decisionTable.Count + (int)multiplier;
+      }
+
+      for (var count = multiplier; count > 0; count--)
+      {
+        this.decisionTable.Add(id);
+      }
+    }
+
+    public int DetermineDecision()
+    {
+      var n = this.numberGenerator.GetRandomNumberBetweenZeroAndMaximum(this.decisionTable.Count);
+      return this.decisionTable[n];
+    }
+  }
 }
