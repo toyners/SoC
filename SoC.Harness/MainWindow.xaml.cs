bbd434f7d77419c748f70548b13ee178e4ff264c
Benjamin Toynbee <toyners@hotmail.com>
2018-10-05 19:06:38 +1000

    Refactoring: new files for classes

--- SoC.Harness/MainWindow.xaml.cs
+++ SoC.Harness/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace SoC.Harness
       this.PlayArea.EndTurnEvent = this.EndTurnEventHandler;
       this.PlayArea.StartGameEvent = this.StartGameEventHandler;
 
-      this.localGameController = new LocalGameController(new NumberGenerator(), new TestPlayerPool());
+      this.localGameController = new LocalGameController(new NumberGenerator(), new PlayerPool());
       this.localGameController.GameJoinedEvent = this.GameJoinedEventHandler;
       this.localGameController.InitialBoardSetupEvent = this.InitialBoardSetupEventHandler;
     }
@@ -60,41 +60,6 @@ namespace SoC.Harness
     private void InitialBoardSetupEventHandler(GameBoard board)
     {
       this.PlayArea.Initialise(board);
-    }
-  }
-
-  public class TestPlayerPool : IPlayerPool
-  {
-    private Queue<string> names = new Queue<string>(new[] { "Barbara", "Charlie", "Dana" });
-
-    public IPlayer CreateComputerPlayer(GameBoard gameBoard)
-    {
-      return new ComputerPlayer(this.names.Dequeue(), gameBoard, null, null);
-    }
-
-    public IPlayer CreateComputerPlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data, GameBoard board, INumberGenerator numberGenerator)
-    {
-      throw new NotImplementedException();
-    }
-
-    public IPlayer CreatePlayer()
-    {
-      return new Player("Player");
-    }
-
-    public IPlayer CreatePlayer(XmlReader reader)
-    {
-      throw new NotImplementedException();
-    }
-
-    public IPlayer CreatePlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data)
-    {
-      throw new NotImplementedException();
-    }
-
-    public Guid GetBankId()
-    {
-      throw new NotImplementedException();
     }
   }
 }
---
+++ SoC.Harness/PlayerPool.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using Jabberwocky.SoC.Library;
+using Jabberwocky.SoC.Library.GameBoards;
+using Jabberwocky.SoC.Library.Interfaces;
+using Jabberwocky.SoC.Library.Storage;
+
+namespace SoC.Harness
+{
+  public class PlayerPool : IPlayerPool
+  {
+    private Queue<string> names = new Queue<string>(new[] { "Barbara", "Charlie", "Dana" });
+
+    public IPlayer CreateComputerPlayer(GameBoard gameBoard)
+    {
+      return new ComputerPlayer(this.names.Dequeue(), gameBoard, null, null);
+    }
+
+    public IPlayer CreateComputerPlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data, GameBoard board, INumberGenerator numberGenerator)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IPlayer CreatePlayer()
+    {
+      return new Player("Player");
+    }
+
+    public IPlayer CreatePlayer(XmlReader reader)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IPlayer CreatePlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Guid GetBankId()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
