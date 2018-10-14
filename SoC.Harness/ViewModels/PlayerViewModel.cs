b208d526a5247cd804d007ece934d950c490abb5
Benjamin Toynbee <toyners@hotmail.com>
2018-10-14 14:13:58 +1100

    History wip

--- SoC.Harness/ViewModels/PlayerViewModel.cs
+++ SoC.Harness/ViewModels/PlayerViewModel.cs
@@ -2,26 +2,35 @@
 namespace SoC.Harness.ViewModels
 {
   using System;
+  using System.Collections.Generic;
   using Jabberwocky.SoC.Library;
   using Jabberwocky.Toolkit.WPF;
 
   public class PlayerViewModel : NotifyPropertyChangedBase
   {
     private ResourceClutch resources;
-    private string resourceSummary;
+    private string resourceText;
+    private Queue<string> historyLines = new Queue<string>();
+    private string historyText;
 
     public PlayerViewModel(PlayerDataModel playerModel, string iconPath)
     {
       this.Name = playerModel.Name;
       this.IconPath = iconPath;
+      this.UpdateHistory(this.Name + " initialised");
     }
 
-    public string Name { get; private set; }
-    public string IconPath { get; private set; }
-    public string ResourceSummary
+    public string HistoryText
     {
-      get { return this.resourceSummary; }
-      private set { this.SetField(ref this.resourceSummary, value); }
+      get { return this.historyText; }
+      private set { this.SetField(ref this.historyText, value); }
+    }
+    public string IconPath { get; private set; }
+    public string Name { get; private set; }
+    public string ResourceText
+    {
+      get { return this.resourceText; }
+      private set { this.SetField(ref this.resourceText, value); }
     }
 
     public void Update(PlayerDataModel playerModel)
@@ -32,12 +41,52 @@ namespace SoC.Harness.ViewModels
     public void Update(ResourceClutch resources)
     {
       this.resources += resources;
-      this.ResourceSummary = 
+      this.ResourceText = 
         $"B{this.resources.BrickCount} " +
         $"G{this.resources.GrainCount} " +
         $"L{this.resources.LumberCount} " +
         $"O{this.resources.OreCount} " +
         $"W{this.resources.WoolCount}";
+
+      var line = "Received ";
+      if (resources.BrickCount > 0)
+      {
+        line += "B" + resources.BrickCount + " ";
+      }
+
+      if (resources.GrainCount > 0)
+      {
+        line += "G" + resources.GrainCount + " ";
+      }
+
+      if (resources.LumberCount > 0)
+      {
+        line += "L" + resources.LumberCount + " ";
+      }
+
+      if (resources.OreCount > 0)
+      {
+        line += "O" + resources.OreCount + " ";
+      }
+
+      if (resources.WoolCount > 0)
+      {
+        line += "W" + resources.WoolCount + " ";
+      }
+
+      this.UpdateHistory(line);
+    }
+
+    public void UpdateHistory(string line)
+    {
+      if (this.historyLines.Count >= 150)
+      {
+        this.historyLines.Dequeue();
+      }
+
+      this.historyLines.Enqueue(line);
+
+      this.HistoryText = string.Join("\n", this.historyLines);
     }
   }
 }
