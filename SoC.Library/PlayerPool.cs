37a86c57925dc4a0f20340419e4bf99920781e08
Benjamin Toynbee <toyners@hotmail.com>
2018-11-16 12:37:32 +1100

    Refactoring only

--- SoC.Library/PlayerPool.cs
+++ SoC.Library/PlayerPool.cs
@@ -1,73 +1,74 @@
 ﻿
 namespace Jabberwocky.SoC.Library
 {
-  using System;
-  using System.Xml;
-  using Interfaces;
-  using GameBoards;
-  using System.Collections.Generic;
-  using Jabberwocky.SoC.Library.Store;
+    using System;
+    using System.Xml;
+    using Interfaces;
+    using GameBoards;
+    using System.Collections.Generic;
+    using Jabberwocky.SoC.Library.Store;
 
-  public class PlayerPool : IPlayerPool
-  {
-    private Guid bankId = Guid.NewGuid();
-    private Queue<String> names = new Queue<String>(new[] { "Bob", "Carol", "Dana" });
-
-    /// <summary>
-    /// Create a computer player instance
-    /// </summary>
-    /// <param name="gameBoard">Game board instance for use in decision making.</param>
-    /// <returns>Computer player instance.</returns>
-    public IPlayer CreateComputerPlayer(GameBoard gameBoard, INumberGenerator numberGenerator)
+    public class PlayerPool : IPlayerPool
     {
-      return new ComputerPlayer(this.names.Dequeue(), gameBoard, numberGenerator, null);
-    }
+        private Guid bankId = Guid.NewGuid();
+        private Queue<string> names = new Queue<string>(new[] { "Bob", "Carol", "Dana" });
 
-    public IPlayer CreateComputerPlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data, GameBoard board, INumberGenerator numberGenerator)
-    {
-      return new ComputerPlayer(data, board, numberGenerator);
-    }
+        /// <summary>
+        /// Create a computer player instance
+        /// </summary>
+        /// <param name="gameBoard">Game board instance for use in decision making.</param>
+        /// <returns>Computer player instance.</returns>
+        public IPlayer CreateComputerPlayer(GameBoard gameBoard, INumberGenerator numberGenerator)
+        {
+            return new ComputerPlayer(this.names.Dequeue(), gameBoard, numberGenerator, null);
+        }
 
-    /// <summary>
-    /// Create a player instance.
-    /// </summary>
-    /// <returns>Player instance</returns>
-    public IPlayer CreatePlayer()
-    {
-      return new Player();
-    }
+        public IPlayer CreateComputerPlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data, GameBoard board, INumberGenerator numberGenerator)
+        {
+            return new ComputerPlayer(data, board, numberGenerator);
+        }
 
-    /// <summary>
-    /// Create a player instance from XML reader.
-    /// </summary>
-    /// <param name="reader">Xml reader containing player data.</param>
-    /// <returns>Player instance</returns>
-    public IPlayer CreatePlayer(XmlReader reader)
-    {
-      var isComputer = false;
-      var isComputerValue = reader.GetAttribute("iscomputer");
-      if (!String.IsNullOrEmpty(isComputerValue) && Boolean.TryParse(isComputerValue, out isComputer) && isComputer)
-      {
-        return ComputerPlayer.CreateFromXML(reader);
-      }
+        /// <summary>
+        /// Create a player instance.
+        /// </summary>
+        /// <returns>Player instance</returns>
+        public IPlayer CreatePlayer()
+        {
+            return new Player();
+        }
 
-      var player = new Player();
-      player.Load(reader);
-      return player;
-    }
+        /// <summary>
+        /// Create a player instance from XML reader.
+        /// </summary>
+        /// <param name="reader">Xml reader containing player data.</param>
+        /// <returns>Player instance</returns>
+        public IPlayer CreatePlayer(XmlReader reader)
+        {
+            var isComputer = false;
+            var isComputerValue = reader.GetAttribute("iscomputer");
+            if (!string.IsNullOrEmpty(isComputerValue) && bool.TryParse(isComputerValue, out isComputer) && isComputer)
+            {
+                return ComputerPlayer.CreateFromXML(reader);
+            }
 
-    public IPlayer CreatePlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data)
-    {
-      return new Player(data);
-    }
+            var player = new Player();
+            player.Load(reader);
+            return player;
+        }
 
-    /// <summary>
-    /// Get the id of the bank
-    /// </summary>
-    /// <returns>Bank id</returns>
-    public Guid GetBankId()
-    {
-      return this.bankId;
+        [Obsolete("Deprecated.")]
+        public IPlayer CreatePlayer(IGameDataSection<GameDataSectionKeys, GameDataValueKeys, ResourceTypes> data)
+        {
+            return new Player(data);
+        }
+
+        /// <summary>
+        /// Get the id of the bank
+        /// </summary>
+        /// <returns>Bank id</returns>
+        public Guid GetBankId()
+        {
+            return this.bankId;
+        }
     }
-  }
 }
