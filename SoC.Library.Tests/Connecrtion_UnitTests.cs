ef7c0265c0a8d2f1ac4a88586b4089e012a2b6ed
Benjamin Toynbee <toyners@hotmail.com>
2018-11-19 23:13:25 +1300

    Connection class to replace RoadSegment class

--- SoC.Library.Tests/Connecrtion_UnitTests.cs
+++ SoC.Library.Tests/Connecrtion_UnitTests.cs
@@ -6,34 +6,34 @@ namespace Jabberwocky.SoC.Library.UnitTests
   using Shouldly;
 
   [TestFixture]
-  public class RoadSegment_UnitTests
+  public class Connection_UnitTests
   {
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void Cstr_LocationsAreSame_ThrowMeaningfulException()
     {
-      Action action = () => { new RoadSegment(0u, 0u); };
+      Action action = () => { new Connection(0u, 0u); };
 
       Should.Throw<ArgumentException>(action).Message.ShouldBe("Locations cannot be the same.");
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void Cstr_ValidRoadParameters_NoExceptionThrown()
     {
-      Action action = () => { new RoadSegment(0, 1); };
+      Action action = () => { new Connection(0, 1); };
 
       action.ShouldNotThrow();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void Equals_OtherObjectIsNotRoad_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
+      var road1 = new Connection(0, 1);
       var road2 = new System.Drawing.Point(0, 1);
 
       road1.Equals(road2).ShouldBeFalse();
@@ -41,10 +41,10 @@ namespace Jabberwocky.SoC.Library.UnitTests
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
-    public void EqualityOperator_RoadSegmentsAreSameObject_ReturnTrue()
+    [Category("Connection")]
+    public void EqualityOperator_ConnectionsAreSameObject_ReturnTrue()
     {
-      var road1 = new RoadSegment(0, 1);
+      var road1 = new Connection(0, 1);
       var road2 = road1;
 
       (road1 == road2).ShouldBeTrue();
@@ -52,131 +52,131 @@ namespace Jabberwocky.SoC.Library.UnitTests
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
-    public void EqualityOperator_OneRoadSegmentIsNull_ReturnFalse()
+    [Category("Connection")]
+    public void EqualityOperator_OneConnectionIsNull_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = (RoadSegment)null;
+      var road1 = new Connection(0, 1);
+      var road2 = (Connection)null;
 
       (road1 == road2).ShouldBeFalse();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
-    public void EqualityOperator_OtherRoadSegmentIsNull_ReturnFalse()
+    [Category("Connection")]
+    public void EqualityOperator_OtherConnectionIsNull_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = (RoadSegment)null;
+      var road1 = new Connection(0, 1);
+      var road2 = (Connection)null;
 
       (road2 == road1).ShouldBeFalse();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
-    public void EqualityOperator_BothRoadSegmentsAreNull_ReturnTrue()
+    [Category("Connection")]
+    public void EqualityOperator_BothConnectionsAreNull_ReturnTrue()
     {
-      ((RoadSegment)null == (RoadSegment)null).ShouldBeTrue();
+      ((Connection)null == (Connection)null).ShouldBeTrue();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void EqualityOperator_LocationsAreSame_ReturnTrue()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(0, 1);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(0, 1);
 
       (road1 == road2).ShouldBeTrue();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void EqualityOperator_LocationsAreDifferent_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(1, 2);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(1, 2);
 
       (road1 == road2).ShouldBeFalse();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void EqualityOperator_LocationsAreDiametricallyIdentical_ReturnTrue()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(1, 0);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(1, 0);
 
       (road1 == road2).ShouldBeTrue();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void InequalityOperator_LocationsAreSame_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(0, 1);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(0, 1);
 
       (road1 != road2).ShouldBeFalse();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void InequalityOperator_LocationsAreDifferent_ReturnTrue()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(1, 2);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(1, 2);
 
       (road1 != road2).ShouldBeTrue();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void InequalityOperator_LocationsAreDiametricallyIdentical_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(1, 0);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(1, 0);
 
       (road1 != road2).ShouldBeFalse();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     [TestCase(0u, 1u, 1u, 2u)]
     [TestCase(1u, 0u, 1u, 2u)]
     public void IsConnected_RoadsShareALocation_ReturnTrue(UInt32 location1, UInt32 location2, UInt32 location3, UInt32 location4)
     {
-      var road1 = new RoadSegment(location1, location2);
-      var road2 = new RoadSegment(location3, location4);
+      var road1 = new Connection(location1, location2);
+      var road2 = new Connection(location3, location4);
 
       road1.IsConnected(road2).ShouldBeTrue();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void IsConnected_RoadsDoNotShareALocation_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(2, 3);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(2, 3);
 
       road1.IsConnected(road2).ShouldBeFalse();
     }
 
     [Test]
     [Category("All")]
-    [Category("RoadSegment")]
+    [Category("Connection")]
     public void IsConnected_SameRoad_ReturnFalse()
     {
-      var road1 = new RoadSegment(0, 1);
-      var road2 = new RoadSegment(1, 0);
+      var road1 = new Connection(0, 1);
+      var road2 = new Connection(1, 0);
 
       road1.IsConnected(road2).ShouldBeFalse();
     }
---
+++ SoC.Library/Connection.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+    using System;
+
+    public class Connection
+    {
+        #region Fields
+        public readonly uint Location1;
+        public readonly uint Location2;
+        #endregion
+
+        #region Construction
+        public Connection(uint location1, uint location2)
+        {
+            if (location1 == location2)
+            {
+                throw new ArgumentException("Locations cannot be the same.");
+            }
+
+            this.Location1 = location1;
+            this.Location2 = location2;
+        }
+        #endregion
+
+        #region Methods
+        public static bool operator ==(Connection connection1, Connection connection2)
+        {
+            if (Object.ReferenceEquals(connection1, null) && Object.ReferenceEquals(connection2, null))
+            {
+                return true;
+            }
+
+            if (Object.ReferenceEquals(connection1, null) || Object.ReferenceEquals(connection2, null))
+            {
+                return false;
+            }
+
+            if (connection1.Location1 == connection2.Location1 && connection1.Location2 == connection2.Location2)
+            {
+                return true;
+            }
+
+            if (connection1.Location1 == connection2.Location2 && connection1.Location2 == connection2.Location1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Connection connection1, Connection connection2)
+        {
+            return !(connection1 == connection2);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (!(obj is Connection))
+            {
+                return false;
+            }
+
+            return this == (Connection)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns true if another connection is connected (i.e. shares only one location) to this connection.
+        /// If the other connection is the same as this connection then false is returned.
+        /// </summary>
+        /// <param name="road">Another road segment.</param>
+        /// <returns>True if connected; otherwise false.</returns>
+        public bool IsConnected(Connection connection)
+        {
+            if (this == connection)
+            {
+                return false;
+            }
+
+            return this.Location1 == connection.Location1 || this.Location1 == connection.Location2 || this.Location2 == connection.Location1 || this.Location2 == connection.Location2;
+        }
+
+        /// <summary>
+        /// Returns true if this road segment is on the location passed in.
+        /// </summary>
+        /// <param name="location">Location to check.</param>
+        /// <returns>True if this road segment is on the location; otherwise false.</returns>
+        public bool IsOnLocation(uint location)
+        {
+            return this.Location1 == location || this.Location2 == location;
+        }
+        #endregion
+    }
+}
