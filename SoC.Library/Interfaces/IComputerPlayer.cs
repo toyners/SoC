bc1d6aad741fd37e9986594f692a4c07c03375be
Benjamin Toynbee <toyners@hotmail.com>
2018-11-28 17:32:27 +1300

    Refactoring only

--- SoC.Library/Interfaces/IComputerPlayer.cs
+++ SoC.Library/Interfaces/IComputerPlayer.cs
@@ -1,28 +1,27 @@
 ﻿
 namespace Jabberwocky.SoC.Library.Interfaces
 {
-  using System;
-  using System.Collections.Generic;
-  using GameActions;
+    using System.Collections.Generic;
+    using GameActions;
     using Jabberwocky.SoC.Library.PlayerData;
 
     public interface IComputerPlayer : IPlayer
-  {
-    #region Methods
-    void AddDevelopmentCard(DevelopmentCard developmentCard);
-    void BuildInitialPlayerActions(PlayerDataModel[] playerData);
-    UInt32 ChooseCityLocation();
-    UInt32 ChooseSettlementLocation();
-    void ChooseInitialInfrastructure(out UInt32 settlementLocation, out UInt32 roadEndLocation);
-    KnightDevelopmentCard ChooseKnightCard();
-    MonopolyDevelopmentCard ChooseMonopolyCard();
-    ResourceClutch ChooseResourcesToCollectFromBank();
-    ResourceClutch ChooseResourcesToDrop();
-    ResourceTypes ChooseResourceTypeToRob();
-    UInt32 ChooseRobberLocation();
-    IPlayer ChoosePlayerToRob(IEnumerable<IPlayer> otherPlayers);
-    ComputerPlayerAction GetPlayerAction();
-    YearOfPlentyDevelopmentCard ChooseYearOfPlentyCard();
-    #endregion
-  }
+    {
+        #region Methods
+        void AddDevelopmentCard(DevelopmentCard developmentCard);
+        void BuildInitialPlayerActions(PlayerDataModel[] playerData);
+        uint ChooseCityLocation();
+        uint ChooseSettlementLocation();
+        void ChooseInitialInfrastructure(out uint settlementLocation, out uint roadEndLocation);
+        KnightDevelopmentCard ChooseKnightCard();
+        MonopolyDevelopmentCard ChooseMonopolyCard();
+        ResourceClutch ChooseResourcesToCollectFromBank();
+        ResourceClutch ChooseResourcesToDrop();
+        ResourceTypes ChooseResourceTypeToRob();
+        uint ChooseRobberLocation();
+        IPlayer ChoosePlayerToRob(IEnumerable<IPlayer> otherPlayers);
+        ComputerPlayerAction GetPlayerAction();
+        YearOfPlentyDevelopmentCard ChooseYearOfPlentyCard();
+        #endregion
+    }
 }
