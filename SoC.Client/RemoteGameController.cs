9f14c58109a417d7d1bf8099147e1cbc73c7caba
Benjamin Toynbee <toyners@hotmail.com>
2018-11-28 17:17:35 +1300

    Refactoring only

--- SoC.Client/RemoteGameController.cs
+++ SoC.Client/RemoteGameController.cs
@@ -9,171 +9,171 @@ using Jabberwocky.SoC.Library.PlayerData;
 
 namespace Jabberwocky.SoC.Client
 {
-  public class RemoteGameController : IGameController, IServiceProviderCallback
-  {
-    #region Fields
-    private ServiceProviderClient serviceProviderClient;
-    #endregion
-
-    public Guid GameId
+    public class RemoteGameController : IGameController, IServiceProviderCallback
     {
-      get
-      {
-        throw new NotImplementedException();
-      }
+        #region Fields
+        private ServiceProviderClient serviceProviderClient;
+        #endregion
+
+        public Guid GameId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #region Events
+        public Action<GameBoardUpdate> BoardUpdatedEvent { get; set; }
+
+        public Action<PlayerDataBase[]> GameJoinedEvent { get; set; }
+
+        public Action<GameBoard> InitialBoardSetupEvent { get; set; }
+
+        public Action<ClientAccount> LoggedInEvent { get; set; }
+
+        public Action<GameBoardUpdate> StartInitialSetupTurnEvent { get; set; }
+        #endregion
+
+        public void AcceptOffer(Offer offer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildRoad(Location startingLocation, Location finishingLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DevelopmentCard BuyDevelopmentCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChooseTownLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmGameIsOver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmGameJoined(Guid gameToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmGameLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmGameSessionJoined(Guid gameToken, GameSessionManagerGameStates gameSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmGameSessionReadyToLaunch()
+        {
+            this.GameJoinedEvent?.Invoke(new[] { new PlayerDataModel(null) });
+        }
+
+        public void ConfirmOtherPlayerHasLeftGame(String username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConfirmPlayerHasLeftGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GameJoined()
+        {
+            this.GameJoinedEvent?.Invoke(new[] { new PlayerDataModel(null) });
+        }
+
+        public void InitializeGame(GameInitializationData gameData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Offer> MakeOffer(Offer offer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceTown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceTown(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayerDataForJoiningClient(ServiceReference.PlayerData playerData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceivePersonalMessage(String sender, String text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JoinGame(GameOptions gameOptions)
+        {
+            var instanceContext = new InstanceContext(this);
+            //this.serviceProviderClient = new ServiceProviderClient(instanceContext, "WSDualHttpBinding_IServiceProvider");
+            var binding = new WSDualHttpBinding();
+            var uri = new Uri("http://localhost:8733/Design_Time_Addresses/Jabberwocky.SoC.Service/IServiceProvider/");
+            var endPointAddress = new EndpointAddress(uri);
+            this.serviceProviderClient = new ServiceProviderClient(instanceContext, binding, endPointAddress);
+            this.serviceProviderClient.TryJoinGameNew();
+        }
+
+        public void StartJoiningGame(GameOptions gameFilter, Guid accountToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartLogIntoAccount(String username, String password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTurn(Guid token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TownPlacedDuringSetup(UInt32 locationIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResourceTypes TradeResourcesAtPort(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResourceTypes TradeResourcesWithBank()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpgradeToCity(Location location)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    #region Events
-    public Action<GameBoardUpdate> BoardUpdatedEvent { get; set; }
-
-    public Action<PlayerDataBase[]> GameJoinedEvent { get; set; }
-
-    public Action<GameBoard> InitialBoardSetupEvent { get; set; }
-
-    public Action<ClientAccount> LoggedInEvent { get; set; }
-
-    public Action<GameBoardUpdate> StartInitialSetupTurnEvent { get; set; }
-    #endregion
-
-    public void AcceptOffer(Offer offer)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void BuildRoad(Location startingLocation, Location finishingLocation)
-    {
-      throw new NotImplementedException();
-    }
-
-    public DevelopmentCard BuyDevelopmentCard()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ChooseTownLocation()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ConfirmGameIsOver()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ConfirmGameJoined(Guid gameToken)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ConfirmGameLeft()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ConfirmGameSessionJoined(Guid gameToken, GameSessionManagerGameStates gameSession)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ConfirmGameSessionReadyToLaunch()
-    {
-      this.GameJoinedEvent?.Invoke(new[] { new PlayerDataModel(null) });
-    }
-
-    public void ConfirmOtherPlayerHasLeftGame(String username)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ConfirmPlayerHasLeftGame()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void GameJoined()
-    {
-      this.GameJoinedEvent?.Invoke(new[] { new PlayerDataModel(null) });
-    }
-
-    public void InitializeGame(GameInitializationData gameData)
-    {
-      throw new NotImplementedException();
-    }
-
-    public ICollection<Offer> MakeOffer(Offer offer)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void PlaceTown()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void PlaceTown(Location location)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void PlayerDataForJoiningClient(ServiceReference.PlayerData playerData)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void Quit()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void ReceivePersonalMessage(String sender, String text)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void JoinGame(GameOptions gameOptions)
-    {
-      var instanceContext = new InstanceContext(this);
-      //this.serviceProviderClient = new ServiceProviderClient(instanceContext, "WSDualHttpBinding_IServiceProvider");
-      var binding = new WSDualHttpBinding();
-      var uri = new Uri("http://localhost:8733/Design_Time_Addresses/Jabberwocky.SoC.Service/IServiceProvider/");
-      var endPointAddress = new EndpointAddress(uri);
-      this.serviceProviderClient = new ServiceProviderClient(instanceContext, binding, endPointAddress);
-      this.serviceProviderClient.TryJoinGameNew();
-    }
-
-    public void StartJoiningGame(GameOptions gameFilter, Guid accountToken)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void StartLogIntoAccount(String username, String password)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void StartTurn(Guid token)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void TownPlacedDuringSetup(UInt32 locationIndex)
-    {
-      throw new NotImplementedException();
-    }
-
-    public ResourceTypes TradeResourcesAtPort(Location location)
-    {
-      throw new NotImplementedException();
-    }
-
-    public ResourceTypes TradeResourcesWithBank()
-    {
-      throw new NotImplementedException();
-    }
-
-    public void UpgradeToCity(Location location)
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
