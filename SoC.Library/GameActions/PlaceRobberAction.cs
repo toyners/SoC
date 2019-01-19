1bc4e709158d8a64e7774e46974c620626f69eab
Benjamin Toynbee <toyners@hotmail.com>
2019-01-19 23:35:21 +1100

    Refactoring only

--- SoC.Library/GameActions/PlaceRobberAction.cs
+++ SoC.Library/GameActions/PlaceRobberAction.cs
@@ -3,10 +3,15 @@ namespace Jabberwocky.SoC.Library.GameActions
 {
     public class PlaceRobberAction : ComputerPlayerAction
     {
+        #region Fields
         public readonly uint RobberHex;
+        #endregion
+
+        #region Construction
         public PlaceRobberAction(uint robberHex) : base(0)
         {
             this.RobberHex = robberHex;
         }
+        #endregion
     }
 }
