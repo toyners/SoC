8a30a43e163a108a08ae3f2d38743a1aaae99fd2
Benjamin Toynbee <toyners@hotmail.com>
2019-02-04 21:45:07 +1100

    Architecture wip

---
+++ SoC.Library/LocalGameServer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+    using System.Collections.Concurrent;
+    using System.Threading;
+    using Jabberwocky.SoC.Library.GameActions;
+    using Jabberwocky.SoC.Library.GameBoards;
+    using Jabberwocky.SoC.Library.Interfaces;
+
+    public class LocalGameServer
+    {
+        private bool requestQuit;
+        private ConcurrentQueue<ComputerPlayerAction> actionRequests = new ConcurrentQueue<ComputerPlayerAction>();
+        private INumberGenerator numberGenerator;
+        private IPlayerPool computerPlayerFactory;
+        private GameBoard gameBoard;
+        private IDevelopmentCardHolder developmentCardHolder;
+
+        internal LocalGameServer(INumberGenerator numberGenerator, IPlayerPool computerPlayerFactory, GameBoard gameBoard, IDevelopmentCardHolder developmentCardHolder)
+        {
+            this.numberGenerator = numberGenerator;
+            this.computerPlayerFactory = computerPlayerFactory;
+            this.gameBoard = gameBoard;
+            this.developmentCardHolder = developmentCardHolder;
+        }
+
+        internal void AddPlayerAction(ComputerPlayerAction playerAction)
+        {
+            this.actionRequests.Enqueue(playerAction);
+        }
+
+        internal void Start()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                if (this.requestQuit)
+                    return;
+
+                if (this.actionRequests.TryDequeue(out var playerAction))
+                    this.ProcessPlayerAction(playerAction);
+            }
+        }
+
+        internal void Quit()
+        {
+            this.requestQuit = true;
+        }
+
+        private void ProcessPlayerAction(ComputerPlayerAction playerAction)
+        {
+            
+        }
+    }
+}
---
+++ SoC.Library/NewLocalGameController.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Jabberwocky.SoC.Library
+{
+    using System.Threading.Tasks;
+    using Jabberwocky.SoC.Library.GameBoards;
+    using Jabberwocky.SoC.Library.Interfaces;
+
+    public class NewLocalGameController
+    {
+        private LocalGameServer localGameServer;
+        private Task localGameServerTask;
+        private INumberGenerator numberGenerator;
+        private IPlayerPool computerPlayerFactory;
+        private GameBoard gameBoard;
+        private IDevelopmentCardHolder developmentCardHolder;
+
+        public NewLocalGameController(INumberGenerator numberGenerator, IPlayerPool computerPlayerFactory, GameBoard gameBoard, IDevelopmentCardHolder developmentCardHolder, bool provideFullPlayerData = false)
+        {
+            this.numberGenerator = numberGenerator;
+            this.computerPlayerFactory = computerPlayerFactory;
+            this.gameBoard = gameBoard;
+            this.developmentCardHolder = developmentCardHolder;
+        }
+
+        public void JoinGame(GameOptions gameOptions)
+        {
+            // Validate request
+
+            // Create server with all player initialization
+            this.localGameServer = new LocalGameServer(this.numberGenerator, this.computerPlayerFactory, this.gameBoard, this.developmentCardHolder);
+
+            // Launch server processing on separate thread
+            this.localGameServerTask = Task.Factory.StartNew(() => 
+            {
+                this.localGameServer.Start();
+            });
+        }
+
+        public void Quit()
+        {
+            // Validate request
+            this.localGameServer.Quit();
+        }
+    }
+}
