d93310faafb833398135a02772fcd87e460bf68f
Benjamin Toynbee <toyners@hotmail.com>
2019-02-14 21:46:45 +1100

    Added lightweight game server

---
+++ SoC.Library/GameServer.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Jabberwocky.SoC.Library.GameBoards;
+using Jabberwocky.SoC.Library.Interfaces;
+
+namespace Jabberwocky.SoC.Library
+{
+    public class GameServer
+    {
+        public LocalGameController LaunchGame(INumberGenerator numberGenerator, GameBoard gameBoard, IDevelopmentCardHolder developmentCardHolder, GameOptions gameOptions = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
