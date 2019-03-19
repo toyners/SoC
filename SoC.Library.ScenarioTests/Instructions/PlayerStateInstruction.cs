6bda54f2a4e65bf6d39990bfa0702a10da254e3a
Benjamin Toynbee <toyners@hotmail.com>
2019-03-20 06:46:25 +1100

    Refactoring only: Moved classes to files

---
+++ SoC.Library.ScenarioTests/Instructions/PlayerStateInstruction.cs
@@ -0,0 +1,49 @@
+﻿
+namespace SoC.Library.ScenarioTests.Instructions
+{
+    using System;
+    using System.Collections.Generic;
+    using Jabberwocky.SoC.Library;
+    using Jabberwocky.SoC.Library.GameEvents;
+
+    internal class PlayerStateInstruction : Instruction
+    {
+        private readonly ScenarioRunner runner;
+        private ResourceClutch? expectedResources;
+
+        public PlayerStateInstruction(string playerName, ScenarioRunner runner) : base(playerName)
+        {
+            this.runner = runner;
+        }
+
+        public PlayerStateInstruction HeldCards(DevelopmentCardTypes developmentCardType)
+        {
+            return this;
+        }
+
+        public PlayerStateInstruction VictoryPoints(uint victoryPoints)
+        {
+            return this;
+        }
+
+        public PlayerStateInstruction Resources(ResourceClutch expectedResources)
+        {
+            this.expectedResources = expectedResources;
+            return this;
+        }
+
+        public ScenarioRunner End() { return this.runner; }
+
+        public ActionInstruction GetAction()
+        {
+            return new ActionInstruction(this.PlayerName, ActionInstruction.OperationTypes.RequestState, null);
+        }
+
+        public GameEvent GetEvent(IDictionary<string, Guid> playerIdsByName)
+        {
+            var requestStateEvent = new ScenarioRequestStateEvent(playerIdsByName[this.PlayerName]);
+            requestStateEvent.Resources = this.expectedResources.Value;
+            return requestStateEvent;
+        }
+    }
+}
---
+++ SoC.Library.ScenarioTests/ScenarioEvents/ScenarioRequestStateEvent.cs
@@ -0,0 +1,31 @@
+﻿
+namespace SoC.Library.ScenarioTests.Instructions
+{
+    using System;
+    using Jabberwocky.SoC.Library;
+    using Jabberwocky.SoC.Library.GameEvents;
+
+    internal class ScenarioRequestStateEvent : GameEvent
+    {
+        public ScenarioRequestStateEvent(Guid playerId) : base(playerId)
+        {
+        }
+
+        public ResourceClutch? Resources { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(RequestStateEvent) || this.PlayerId != ((GameEvent)obj).PlayerId)
+                return false;
+
+            var other = (RequestStateEvent)obj;
+            if (this.Resources.HasValue)
+            {
+                if (!this.Resources.Value.Equals(other.Resources))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
