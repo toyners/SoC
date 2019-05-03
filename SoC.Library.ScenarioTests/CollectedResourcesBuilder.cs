e2d0941308e396f8e7179c84aa92fd02328fb935
Benjamin Toynbee <toyners@hotmail.com>
2019-05-03 17:35:07 +1000

    Refactoring only. Moved class to its own file

---
+++ SoC.Library.ScenarioTests/CollectedResourcesBuilder.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SoC.Library.ScenarioTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Jabberwocky.SoC.Library;
+
+    internal class CollectedResourcesBuilder
+    {
+        private Dictionary<string, List<ResourceCollection>> resourceCollectionsByPlayerName = new Dictionary<string, List<ResourceCollection>>();
+
+        public CollectedResourcesBuilder Add(string playerName, uint location, ResourceClutch resources)
+        {
+            if (!this.resourceCollectionsByPlayerName.TryGetValue(playerName, out var resourcesCollection))
+            {
+                resourcesCollection = new List<ResourceCollection>();   
+                this.resourceCollectionsByPlayerName.Add(playerName, resourcesCollection);
+            }
+
+            resourcesCollection.Add(new ResourceCollection(location, resources));
+
+            return this;
+        }
+
+        public Dictionary<string, ResourceCollection[]> Build()
+        {
+            var result = new Dictionary<string, ResourceCollection[]>();
+
+            foreach(var kv in this.resourceCollectionsByPlayerName)
+            {
+                var resourceCollection = kv.Value.OrderBy(k => k.Location).ToArray();
+                result.Add(kv.Key, resourceCollection);
+            }
+
+            return result;
+        }
+    }
+}
