729808b331b17da91b6d49a4f189bfed6b3d1e87
Benjamin Toynbee <toyners@hotmail.com>
2019-05-24 07:09:59 +1000

    Not implemented fix

--- SoC.Library/PlayerPool.cs
+++ SoC.Library/PlayerPool.cs
@@ -64,7 +64,7 @@ namespace Jabberwocky.SoC.Library
 
         public IPlayer CreatePlayer(string name, Guid id)
         {
-            throw new NotImplementedException();
+            return new Player(name, id);
         }
 
         /// <summary>
