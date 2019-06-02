d198e94a7991ca05959964d753947f86afd6121c
Benjamin Toynbee <toyners@hotmail.com>
2019-06-03 06:19:02 +1000

    Fixed compliation error

--- SoC.Library/PlayerActions/PlaceCityAction.cs
+++ SoC.Library/PlayerActions/PlaceCityAction.cs
@@ -1,26 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Jabberwocky.SoC.Library.PlayerActions
 {
     public class PlaceCityAction : PlayerAction
     {
-        public override bool Equals(object obj)
+        public PlaceCityAction(Guid playerId) : base(playerId)
         {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
         }
     }
 }
