d20ed1ed3acdc67f4de59b43070e1804075d187a
Benjamin Toynbee <toyners@hotmail.com>
2019-07-11 06:57:39 +1000

    Html logging

--- SoC.Library.ScenarioTests/PlayerAgentLog.cs
+++ SoC.Library.ScenarioTests/PlayerAgentLog.cs
@@ -15,7 +15,7 @@ namespace SoC.Library.ScenarioTests
             this.logEvents.Add(new MatchedExpectedEvent(actualEvent, expectedEvent));
 
         public void AddActualEvent(GameEvent actualEvent) =>
-            this.logEvents.Add(new UnmatchActualEvent(actualEvent));
+            this.logEvents.Add(new UnmatchedActualEvent(actualEvent));
 
         public void AddUnmatchedExpectedEvent(GameEvent expectedEvent) =>
             this.logEvents.Add(new UnmatchedExpectedEvent(expectedEvent));
@@ -35,8 +35,10 @@ namespace SoC.Library.ScenarioTests
                 "border-collapse: collapse;" +
                 "} " +
                 "td {" +
-                "border: 1px solid black" +
-                "</style></head><body><table>" +
+                "border: 1px solid black } " +
+                ".matched {" +
+                "background-color: green; } " +
+                "</style></head><body><div><table>" +
                 "<tr><th>Actual</th><th>Expected</th></tr>";
             
             foreach (var logEvent in this.logEvents.Where(l => !(l is NoteEvent || l is ExceptionEvent)))
@@ -44,7 +46,14 @@ namespace SoC.Library.ScenarioTests
                 content += logEvent.ToHtml();
             }
 
-            File.WriteAllText(filePath, content + "</table></body>");
+            content += "</table></div><br /><div><table>" +
+                "<tr><th>Actual</th><th>Expected</th></tr>";
+            foreach (var logEvent in this.logEvents)
+            {
+                content += logEvent.ToHtml();
+            }
+
+            File.WriteAllText(filePath, content + "</table></div></body>");
         }
 
         private interface ILogEvent
@@ -52,10 +61,47 @@ namespace SoC.Library.ScenarioTests
             string ToHtml();
         }
 
-        private class UnmatchActualEvent : ILogEvent
+        private class ExceptionEvent : ILogEvent
+        {
+            private Exception exception;
+            public ExceptionEvent(Exception exception) => this.exception = exception;
+
+            public string ToHtml()
+            {
+                return $"<tr><td colspan=\"2\">{this.exception.Message}</td></tr>";
+            }
+        }
+
+        private class MatchedExpectedEvent : ILogEvent
+        {
+            private GameEvent actualEvent, expectedEvent;
+            public MatchedExpectedEvent(GameEvent actualEvent, GameEvent expectedEvent)
+            {
+                this.actualEvent = actualEvent;
+                this.expectedEvent = expectedEvent;
+            }
+
+            public string ToHtml()
+            {
+                return $"<tr class=\"matched\"><td>{this.actualEvent.SimpleTypeName}</td><td>{this.expectedEvent.SimpleTypeName}</td></tr>";
+            }
+        }
+
+        private class NoteEvent : ILogEvent
+        {
+            private string note;
+            public NoteEvent(string note) => this.note = note;
+
+            public string ToHtml()
+            {
+                return $"<tr><td colspan=\"2\">{this.note}</td></tr>";
+            }
+        }
+
+        private class UnmatchedActualEvent : ILogEvent
         {
             private GameEvent actualEvent;
-            public UnmatchActualEvent(GameEvent actualEvent) => this.actualEvent = actualEvent;
+            public UnmatchedActualEvent(GameEvent actualEvent) => this.actualEvent = actualEvent;
 
             public string ToHtml()
             {
@@ -71,43 +117,6 @@ namespace SoC.Library.ScenarioTests
             public string ToHtml()
             {
                 return $"<tr><td></td><td>{this.expectedEvent.SimpleTypeName}</td></tr>";
-            }
-        }
-
-        private class MatchedExpectedEvent : ILogEvent
-        {
-            private GameEvent actualEvent, expectedEvent;
-            public MatchedExpectedEvent(GameEvent actualEvent, GameEvent expectedEvent)
-            {
-                this.actualEvent = actualEvent;
-                this.expectedEvent = expectedEvent;
-            }
-
-            public string ToHtml()
-            {
-                return $"<tr><td>{this.actualEvent.SimpleTypeName}</td><td>{this.expectedEvent.SimpleTypeName}</td></tr>";
-            }
-        }
-
-        private class ExceptionEvent : ILogEvent
-        {
-            private Exception exception;
-            public ExceptionEvent(Exception exception) => this.exception = exception;
-
-            public string ToHtml()
-            {
-                return $"<tr><td colspan=\"2\">{this.exception.Message}</td></tr>";
-            }
-        }
-
-        private class NoteEvent : ILogEvent
-        {
-            private string note;
-            public NoteEvent(string note) => this.note = note;
-
-            public string ToHtml()
-            {
-                return $"<tr><td colspan=\"2\">{this.note}</td></tr>";
             }
         }
     }
