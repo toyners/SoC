098301d2e2c3521ff79601f4fd3f4eee4e2d834a
Benjamin Toynbee <toyners@hotmail.com>
2019-08-28 21:57:43 +1000

    SignalR testbed added

---
+++ SoC.Server.Testbed/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SoC.Server.Testbed.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ SoC.SignalR.Testbed/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SoC.SignalR.Testbed.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await this.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
