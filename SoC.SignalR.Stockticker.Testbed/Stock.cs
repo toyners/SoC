2e59e5361b0f3f35fa875fd6d5717639ea7c01c1
Benjamin Toynbee <toyners@hotmail.com>
2019-08-29 07:01:45 +1000

    Adding stockticker signalr testbed

---
+++ SoC.SignalR.Stockticker.Testbed/Stock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SoC.SignalR.Stockticker.Testbed
+{
+    public class Stock
+    {
+        private decimal _price;
+
+        public string Symbol { get; set; }
+
+        public decimal Price
+        {
+            get
+            {
+                return this._price;
+            }
+            set
+            {
+                if (this._price == value)
+                {
+                    return;
+                }
+
+                this._price = value;
+
+                if (this.DayOpen == 0)
+                {
+                    this.DayOpen = this._price;
+                }
+            }
+        }
+
+        public decimal DayOpen { get; private set; }
+
+        public decimal Change
+        {
+            get
+            {
+                return this.Price - this.DayOpen;
+            }
+        }
+
+        public double PercentChange
+        {
+            get
+            {
+                return (double)Math.Round(this.Change / this.Price, 4);
+            }
+        }
+    }
+}
