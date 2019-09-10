e0d900838ebb184f6c834992e39504d0ed79a867
Benjamin Toynbee <toyners@hotmail.com>
2019-09-11 07:03:15 +1000

    Starting game wip

--- SoC.SignalR.Testbed/GameManager.cs
+++ SoC.SignalR.Testbed/GameManager.cs
@@ -5,6 +5,8 @@ namespace SoC.SignalR.Testbed
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.SignalR;
     using SoC.SignalR.Testbed.Hubs;
 
@@ -12,11 +14,41 @@ namespace SoC.SignalR.Testbed
     {
         private readonly IHubContext<GameHub> hubContext;
         private readonly List<GameDetails> waitingGames = new List<GameDetails>();
-        private readonly Dictionary<Guid, GameDetails> waitingGamesById = new Dictionary<Guid, GameDetails>();
+        private readonly ConcurrentDictionary<Guid, GameDetails> waitingGamesById = new ConcurrentDictionary<Guid, GameDetails>();
         private readonly ConcurrentQueue<GameDetails> startingGames = new ConcurrentQueue<GameDetails>();
+        private readonly ConcurrentDictionary<Guid, GameDetails> startingGamesById = new ConcurrentDictionary<Guid, GameDetails>();
         private readonly ConcurrentDictionary<Guid, GameDetails> inPlayGames = new ConcurrentDictionary<Guid, GameDetails>();
+        private Task startingGameTask;
+        private CancellationToken cancellationToken;
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
-        public GameManager(IHubContext<GameHub> hubContext) => this.hubContext = hubContext;
+        public GameManager(IHubContext<GameHub> hubContext)
+        {
+            this.hubContext = hubContext;
+            this.cancellationToken = this.cancellationTokenSource.Token;
+            this.startingGameTask = Task.Factory.StartNew(o => { this.ProcessStartingGames(); }, this, this.cancellationToken);
+        }
+
+        private void ProcessStartingGames()
+        {
+            try
+            {
+                while (true)
+                {
+                    this.cancellationToken.ThrowIfCancellationRequested();
+                    if (this.startingGames.TryPeek(out var gameDetails))
+                    {
+
+                    }
+
+                    Thread.Sleep(500);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+        }
 
         public CreateGameResponse CreateGame(CreateGameRequest createGameRequest)
         {
@@ -29,7 +61,7 @@ namespace SoC.SignalR.Testbed
             };
             gameInfo.Players.Add(createGameRequest.ConnectionId);
             this.waitingGames.Add(gameInfo);
-            this.waitingGamesById.Add(gameInfo.Id, gameInfo);
+            this.waitingGamesById.TryAdd(gameInfo.Id, gameInfo);
             return new CreateGameResponse(gameInfo.Id);
         }
 
@@ -66,7 +98,9 @@ namespace SoC.SignalR.Testbed
             if (gameDetails.NumberOfSlots == 0)
             {
                 gameDetails.Status = GameStatus.Starting;
-
+                this.waitingGamesById.TryRemove(gameDetails.Id, out var gd);
+                this.startingGamesById.TryAdd(gameDetails.Id, gameDetails);
+                this.startingGames.Enqueue(gameDetails);
             }
 
             return gameDetails.Status;
