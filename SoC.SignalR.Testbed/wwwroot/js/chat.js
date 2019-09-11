1c91e86d4e4f576f9073bd3369b0c2133c1954e8
Benjamin Toynbee <toyners@hotmail.com>
2019-09-11 18:17:54 +1000

    Stopped errors happening in chat.js

--- SoC.SignalR.Testbed/wwwroot/js/chat.js
+++ SoC.SignalR.Testbed/wwwroot/js/chat.js
@@ -14,16 +14,16 @@ connection.on("ReceiveMessage", function (user, message) {
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+    //document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
+/*document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
-});
+});*/
