169630d5fab1e17d9a2f5c750c6fcdf5e8b9a98e
Benjamin Toynbee <toyners@hotmail.com>
2019-09-29 21:50:09 +1000

    Single player game launch wip

--- SoC.WebApplication/Pages/Game.cshtml.cs
+++ SoC.WebApplication/Pages/Game.cshtml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SoC.WebApplication.Pages
 {
     public class GameModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(string gameId, string playerId)
         {
-
+            this.GameId = gameId;
+            this.PlayerId = playerId;
         }
+
+        public string GameId { get; set; }
+        public string PlayerId { get; set; }
     }
 }
