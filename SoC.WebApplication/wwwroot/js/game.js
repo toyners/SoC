4049b4e4a767ce0c3f199b6714cf3b87fe2f96bc
Benjamin Toynbee <toyners@hotmail.com>
2019-10-09 06:57:34 +1100

    Background image now showing

--- SoC.WebApplication/wwwroot/js/game.js
+++ SoC.WebApplication/wwwroot/js/game.js
@@ -23,7 +23,7 @@ function startGame() {
     var state = new Kiwi.State('Play');
     state.preload = function () {
         Kiwi.State.prototype.preload(this);
-        this.addImage('background', './images/background.png');
+        this.addImage('background', '../../../images/background.png');
     };
 
     state.create = function () {
@@ -33,8 +33,8 @@ function startGame() {
     };
 
     var gameOptions = {
-        width: 768,
-        height: 512
+        width: 800,
+        height: 600
     };
 
     game = new Kiwi.Game('game-container', 'soc', state, gameOptions);
