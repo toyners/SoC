12c123f4faaa4d7e27b575cdff94abed1dd34f15
Benjamin Toynbee <toyners@hotmail.com>
2019-10-25 17:48:34 +1100

    - Added redhouse image - Moved preload function to its own file

---
+++ SoC.WebApplication/wwwroot/js/preload.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+function preload() {
+    Kiwi.State.prototype.preload(this);
+    this.addImage('background', '../../../images/background.png');
+    this.addImage('brickhex', '../../../images/hextypes/brick.png');
+    this.addImage('deserthex', '../../../images/hextypes/desert.png');
+    this.addImage('grainhex', '../../../images/hextypes/grain.png');
+    this.addImage('lumberhex', '../../../images/hextypes/lumber.png');
+    this.addImage('orehex', '../../../images/hextypes/ore.png');
+    this.addImage('woolhex', '../../../images/hextypes/wool.png');
+    this.addImage('two', '../../../images/productionfactors/2.png');
+    this.addImage('three', '../../../images/productionfactors/3.png');
+    this.addImage('four', '../../../images/productionfactors/4.png');
+    this.addImage('five', '../../../images/productionfactors/5.png');
+    this.addImage('six', '../../../images/productionfactors/6.png');
+    this.addImage('eight', '../../../images/productionfactors/8.png');
+    this.addImage('nine', '../../../images/productionfactors/9.png');
+    this.addImage('ten', '../../../images/productionfactors/10.png');
+    this.addImage('eleven', '../../../images/productionfactors/11.png');
+    this.addImage('twelve', '../../../images/productionfactors/12.png');
+    this.addImage('redhouse', '../../../images/redhouse.png');
+}
