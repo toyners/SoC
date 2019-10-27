d19ff805d33b2ec2df4cf528c87f2453f2871e6e
Benjamin Toynbee <toyners@hotmail.com>
2019-10-27 21:57:08 +1100

    Refactoring only: Zombie code removed

--- SoC.WebApplication/wwwroot/js/game.js
+++ SoC.WebApplication/wwwroot/js/game.js
@@ -30,61 +30,7 @@ function startGame() {
     var backgroundWidth = 800;
     var backgroundHeight = 600;
 
-    state.create = create; /*function () {
-        Kiwi.State.prototype.create(this);
-        this.background = new Kiwi.GameObjects.Sprite(this, this.textures.background, 0, 0);
-        this.addChild(this.background);
-
-        this.hexSprites = [];
-        this.productionFactorSprites = [];
-
-        var cellHeight = 45;
-        var halfCellHeight = Math.trunc(45 / 2);
-        var halfCellWidth = halfCellHeight;
-        var cellFragmentWidth = 34;
-        var startX = (backgroundWidth / 2) - halfCellWidth;
-        var startY = (backgroundHeight / 2) - halfCellHeight;
-        var x = startX;
-        var y = startY;
-        var index = 0;
-
-        var layoutColumnData = [
-            { x: startX - (2 * cellFragmentWidth), y: startY - cellHeight, count: 3 },
-            { x: startX - cellFragmentWidth, y: startY - halfCellHeight - cellHeight, count: 4 },
-            { x: startX, y: startY - (2 * cellHeight), count: 5 },
-            { x: startX + cellFragmentWidth, y: startY - halfCellHeight - cellHeight, count: 4 },
-            { x: startX + (2 * cellFragmentWidth), y: startY - cellHeight, count: 3 },
-        ];
-
-        for (var columnDataKey in layoutColumnData) {
-            var columnData = layoutColumnData[columnDataKey];
-            x = columnData.x;
-            y = columnData.y;
-            var count = columnData.count;
-            while (count-- > 0) {
-                var hex = hexData[index++];
-                this.hexSprites[index] = new Kiwi.GameObjects.Sprite(this, getResourceTexture(hex.resourceType, this.textures), x, y);
-                this.addChild(this.hexSprites[index]);
-                if (hex.productionFactor != 0) {
-                    this.productionFactorSprites[index] = new Kiwi.GameObjects.Sprite(this, getProductionFactorTexture(hex.productionFactor, this.textures), x, y);
-                    this.addChild(this.productionFactorSprites[index]);
-                }
-                y += cellHeight;
-            }
-        }
-
-        this.firstPlayerName = new Kiwi.GameObjects.Textfield(this, playerNamesInOrder[0], 10, 10, "#000", 32, 'normal', 'Impact');
-        this.addChild(this.firstPlayerName);
-
-        this.secondPlayerName = new Kiwi.GameObjects.Textfield(this, playerNamesInOrder[1], 10, 550, "#000", 32, 'normal', 'Impact');
-        this.addChild(this.secondPlayerName);
-
-        this.thirdPlayerName = new Kiwi.GameObjects.Textfield(this, playerNamesInOrder[2], 700, 10, "#000", 32, 'normal', 'Impact');
-        this.addChild(this.thirdPlayerName);
-
-        this.fourthPlayerName = new Kiwi.GameObjects.Textfield(this, playerNamesInOrder[3], 700, 550, "#000", 32, 'normal', 'Impact');
-        this.addChild(this.fourthPlayerName);
-    };
+    state.create = create; 
 
     state.update = function () {
         Kiwi.State.prototype.update.call(this);
@@ -95,7 +41,7 @@ function startGame() {
                 // Placing infrastructure animation
             }
         }
-    };*/
+    };
 
     var gameOptions = {
         width: backgroundWidth,
