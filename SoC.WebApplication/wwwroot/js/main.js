f41e079db46e78a7ef3fbcc7a2e1a8d8465bedef
Benjamin Toynbee <toyners@hotmail.com>
2019-11-28 22:01:38 +1100

    Enconding data playload as json

--- SoC.WebApplication/wwwroot/js/main.js
+++ SoC.WebApplication/wwwroot/js/main.js
@@ -60,7 +60,7 @@ function main() {
                     gameId: gameId,
                     playerId: playerId,
                     playerActionType: 'PlaceSetupInfrastructureAction',
-                    data: placementData.toString()
+                    data: JSON.stringify(placementData)
                 };
                 this.initialPlacementUI = null;
                 connection.invoke("PlayerAction", request).catch(function (err) {
