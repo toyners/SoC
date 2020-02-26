57d7163dc2c48e1ed831a392954d72aa77d86b16
Benjamin Toynbee <toyners@hotmail.com>
2020-02-27 07:08:20 +1100

    Message line wip

--- SoC.WebApplication/wwwroot/js/game/ui/messageManager.js
+++ SoC.WebApplication/wwwroot/js/game/ui/messageManager.js
@@ -2,21 +2,25 @@
 
 class MessageManager {
     constructor(gameState, layout) {
+        this.messageLineFields = [];
         this.messageLines = [];
 
         for (var coords of layout) {
-            var messageField = new Kiwi.GameObjects.Textfield(gameState, "", coords.x, coords.y, "#000", 16, 'normal', 'impact');
-            this.messageLines.push(messageField);
-            gameState.addChild(messageField);
+            var messageLineField = new Kiwi.GameObjects.Textfield(gameState, "", coords.x, coords.y, "#000", 16, 'normal', 'impact');
+            this.messageLineFields.push(messageLineField);
+            gameState.addChild(messageLineField);
         }
     }
 
     addLine(text) {
-        for (var messageLine of this.messageLines) {
-            if (messageLine.text == "") {
-                messageLine.text = text;
-                break;
+        this.messageLines.push(text);
+        for (var messageLineField of this.messageLineFields) {
+            if (messageLineField.text == "") {
+                messageLineField.text = text;
+                return;
             }
         }
+
+        for ()
     }
 }
