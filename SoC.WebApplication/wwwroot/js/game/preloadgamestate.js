3059048998df708f00f4e9f63c8bcaf448987c4a
Benjamin Toynbee <toyners@hotmail.com>
2020-03-12 07:07:33 +1100

    - Moved existing button images. Enhanced them - Added build button image

--- SoC.WebApplication/wwwroot/js/game/preloadgamestate.js
+++ SoC.WebApplication/wwwroot/js/game/preloadgamestate.js
@@ -2,6 +2,9 @@
 
 function preloadGameState() {
     Kiwi.State.prototype.preload(this);
+
+    var controlImagePath = '../../../images/control/';
+
     this.addImage('background', '../../../images/background.png');
     this.addSpriteSheet('hextypes', '../../../images/hextypes.png', 100, 100);
     this.addSpriteSheet('productionfactors', '../../../images/productionfactors.png', 100, 100);
@@ -11,9 +14,11 @@ function preloadGameState() {
     this.addSpriteSheet('horizontalRoads', '../../../images/road/horizontal_roads.png', 37, 10);
     this.addSpriteSheet('marker', '../../../images/player/playermarker.png', 50, 45);
     this.addSpriteSheet('reverseMarker', '../../../images/player/reverseplayermarker.png', 50, 45);
-    this.addSpriteSheet('confirm', '../../../images/confirm.png', 30, 30);
-    this.addSpriteSheet('cancel', '../../../images/cancel.png', 30, 30);
-    this.addSpriteSheet('end', '../../../images/end.png', 60, 30)
+
+    this.addSpriteSheet('build', controlImagePath + 'build.png', 70, 30)
+    this.addSpriteSheet('confirm', controlImagePath + 'confirm.png', 96, 30);
+    this.addSpriteSheet('cancel', controlImagePath + 'cancel.png', 30, 30);
+    this.addSpriteSheet('end', controlImagePath + 'end.png', 107, 30)
 
     this.addSpriteSheet('resourceTypes', '../../../images/player/resourceTypes.png', 20, 20)
     this.addSpriteSheet('settlementIcons', '../../../images/player/settlementIcons.png', 20, 20)
